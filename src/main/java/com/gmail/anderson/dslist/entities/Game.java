fd818ed14bd2c515a8a5ee366a06e46d6fd6aea0
Anderson reis <anderson.200810@gmail.com>
2023-11-13 23:10:50 -0300

    creation of the entity package and the gamer class with its attributes. The no-argument constructor and its gettes and settes were removed from Lombok

---
+++ src/main/java/com/gmail/anderson/dslist/entities/Game.java
@@ -0,0 +1,29 @@
+package com.gmail.anderson.dslist.entities;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_game")
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    @Column(name = "game_year")
+    private Integer year;
+    private String platforms;
+    private Double score;
+    private String imgUrl;
+    @Column(columnDefinition = "TEXT")
+    private String shortDescription;
+    @Column(columnDefinition = "TEXT")
+    private String longDescription;
+}
